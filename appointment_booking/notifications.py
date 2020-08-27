4002e05dcdc135dfffef2d1de094607d1fca79f5
pibico <pibico.sl@gmail.com>
2020-08-27 11:49:29 +0200

    Indent Correction on Notifications

--- appointment_booking/notifications.py
+++ appointment_booking/notifications.py
@@ -7,12 +7,6 @@ import frappe
 def get_notification_config():
 	return {
 		"for_doctype": {
-			"Error Log": {"seen": 0},
-			"Communication": {"status": "Open", "communication_type": "Communication"},
 			"Visitor Appointment": {"status": "Open"},
-			"ToDo": "frappe.core.notifications.get_things_todo",
-			"Event": "frappe.core.notifications.get_todays_events",
-			"Error Snapshot": {"seen": 0, "parent_error_snapshot": None},
-			"Workflow Action": {"status": 'Open'}
 		},
 	}
